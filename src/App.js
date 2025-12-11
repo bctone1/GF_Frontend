fc0e487a6110a4aa3cc52c9635e119b790855374
Lim Yeong-Bin <dudqls327@naver.com>
2025-12-11 10:12:23 +0900

    라우터 수정

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ export default function App() {
 
         <Route path="/sms" element={<SMS />} />
 
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup/user" element={<UserSignup />} />
         <Route path="/signup/organization" element={<OrganizationSignup />} />
